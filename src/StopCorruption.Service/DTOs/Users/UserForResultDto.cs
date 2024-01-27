8632eb37965eaac6ca725fe9e4a9ac90b1c3e90b
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-27 11:17:00 +0500

    Created Dtos for Users

---
+++ src/StopCorruption.Service/DTOs/Users/UserForResultDto.cs
@@ -0,0 +1,9 @@
+﻿namespace StopCorruption.Service.DTOs.Users;
+
+public class UserForResultDto
+{
+    public bool IsOneID { get; set; }
+    public long OneID { get; set; }
+    public long TelegramId { get; set; }
+
+}
