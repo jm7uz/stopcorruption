d8e3197c8d7c9c8214b89bc9e345cb72d77b7f0e
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-27 12:44:28 +0500

    Created IUserService

---
+++ src/StopCorruption.Service/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using StopCorruption.Service.DTOs.Users;
+
+namespace StopCorruption.Service.Interfaces;
+
+public interface IUserService
+{
+    Task<bool> RemoveAsync(long Id);
+    Task<UserForResultDto> RetrieveByIdAsync(long id);
+    Task<IEnumerable<UserForResultDto>> RetrieveAllAsync();
+    Task<UserForResultDto> AddAsync(UserForCreationDto dto);
+    Task<UserForResultDto> ModifyAsync(long id, UserForUpdateDto dto);
+}
