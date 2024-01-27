1203eb66c35320d0c45b70946b0e3c73ac3a64b9
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-27 13:59:23 +0500

    MiddleWare was created to log errors

---
+++ src/StopCorruption.Api/Middlewares/ExceptionHandlerMiddleWare.cs
@@ -0,0 +1,44 @@
+﻿using StopCorruption.Api.Models;
+using StopCorruption.Service.Exceptions;
+
+namespace StopCorruption.Api.Middlewares;
+
+public class ExceptionHandlerMiddleWare
+{
+    private readonly RequestDelegate next;
+    private readonly ILogger<ExceptionHandlerMiddleWare> logger;
+
+    public ExceptionHandlerMiddleWare(RequestDelegate next, ILogger<ExceptionHandlerMiddleWare> logger)
+    {
+        this.next = next;
+        this.logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (CorruptionException ex)
+        {
+            this.logger.LogError(ex.Message);
+            context.Response.StatusCode = ex.StatusCode;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = ex.StatusCode,
+                Message = ex.Message,
+            });
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError($"{ex}\n\n");
+            context.Response.StatusCode = 500;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = 500,
+                Message = ex.Message,
+            });
+        }
+    }
+}
