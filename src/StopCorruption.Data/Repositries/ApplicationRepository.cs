400a3dfee7934c31efbeb7071375eb7f908844dc
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-27 16:05:50 +0500

    Created ApplicationRepositories

---
+++ src/StopCorruption.Data/Repositries/ApplicationRepository.cs
@@ -0,0 +1,12 @@
+﻿using StopCorruption.Data.DbContexts;
+using StopCorruption.Data.IRepositories;
+using StopCorruption.Domain.Entities;
+
+namespace StopCorruption.Data.Repositries;
+
+public class ApplicationRepository : Repository<Application>, IApplicationRepository
+{
+    public ApplicationRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+}
