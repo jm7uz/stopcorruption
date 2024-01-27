20f6f7718945fd6fb70c5d2071e040bd8535b22b
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-27 14:02:21 +0500

    Created Extensions

---
+++ src/StopCorruption.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using StopCorruption.Data.IRepositories;
+using StopCorruption.Data.Repositries;
+using StopCorruption.Service.Interfaces;
+using StopCorruption.Service.Services;
+
+namespace StopCorruption.Api.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void AddCorruptionService(this IServiceCollection services)
+    {
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+
+
+    }
+}
