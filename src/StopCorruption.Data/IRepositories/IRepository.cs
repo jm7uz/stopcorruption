e810317a529193eba7de2ea92fb5ffc9f43fa0f1
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-27 10:41:59 +0500

    Created IRepository and Repository

---
+++ src/StopCorruption.Data/IRepositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using StopCorruption.Domain.Commons;
+using System.Security.AccessControl;
+
+namespace StopCorruption.Data.IRepositories;
+
+public interface IRepository<TEntity> where TEntity : Auditable
+{
+    Task<bool> DeleteAsync(long id);
+    IQueryable<TEntity> SelectAll();
+    Task<TEntity> SelectByIdAsync(long id);
+    Task<TEntity> InsertAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+}
