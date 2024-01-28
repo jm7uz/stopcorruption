2a1d3c26b63e2aecbdcc4181477a7f0731c20804
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-01-28 11:00:46 +0500

    Updated user information and added Migration

--- src/StopCorruption.Domain/Entities/User.cs
+++ src/StopCorruption.Domain/Entities/User.cs
@@ -6,8 +6,14 @@ namespace StopCorruption.Domain.Entities;
 public class User : Auditable
 {
     public bool IsOneID { get; set; }
-    public string OneID {  get; set; }
     public long TelegramId { get; set; }
+    public string FullName { get; set; }
+    public string PermitAddress { get; set; }
+    public string Phone { get; set; }
+    public string Language { get; set; }
+    public string passportNumber { get; set; }
+    public string passportIssuedBy { get; set; }
+
     [JsonIgnore]
     public ICollection<Application> Applications { get; set; }
 }
--- src/StopCorruption.Service/DTOs/Users/UserForResultDto.cs
+++ src/StopCorruption.Service/DTOs/Users/UserForResultDto.cs
@@ -6,7 +6,12 @@ public class UserForResultDto
 {
     public long Id { get; set; }
     public bool IsOneID { get; set; }
-    public string OneID { get; set; }
     public long TelegramId { get; set; }
+    public string FullName { get; set; }
+    public string PermitAddress { get; set; }
+    public string Phone { get; set; }
+    public string Language { get; set; }
+    public string passportNumber { get; set; }
+    public string passportIssuedBy { get; set; }
     public ICollection<ApplicationForResultDto> Application { get; set; }
 }
